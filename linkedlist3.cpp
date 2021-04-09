407f5544d5864de13272b8f35e53c3ed69c01736
Sahil35077 <53940559+Sahil35077@users.noreply.github.com>
2021-04-09 21:03:34 +0530

    insertingAtNth.cpp

---
+++ linkedlist3.cpp
@@ -0,0 +1,48 @@
+//inserting node at nth position in linked list
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct node
+{
+  int data;
+  node* next;
+};
+node*head;
+void insert(int data,int n)
+{
+  node*temp1=new node();
+  temp1->data=data;
+  temp1->next=NULL;
+  if(n==1)
+  {
+    temp1->next=head;
+    head=temp1;
+    return;
+  }
+  node* temp2=head;
+  for(int i=0;i<n-2;i++)
+  {
+    temp2=temp2->next;
+  }
+  temp1->next=temp2->next;
+  temp2->next=temp1;
+}
+void print()
+{
+  node*temp=head;
+  while(temp!=NULL)
+  {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+  }
+  cout<<"\n";
+}
+int main()
+{
+  head=NULL;
+  insert(2,1);
+  insert(3,2);
+  insert(4,1);
+  insert(5,2);
+  print();
+}
